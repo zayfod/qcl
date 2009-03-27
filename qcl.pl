91a49c11fd2768629de1ee19ebc11dd427c4812c
Kaloyan Tenchov <zayfod@gmail.com>
2009-03-27 16:36:31 +0000

    - Added documentation. - Improved the working of the --help option.

--- qcl.pl
+++ qcl.pl
@@ -31,12 +31,15 @@
 #   - Parse error messages of file service.
 #   - Allow multiple sources of put and get commands.
 #   - Recursive deletion of directories.
+#   - ls command.
+#   - ps command.
+#   - Handle console input.
 #
 
 use warnings;
 use strict;
 
-use Getopt::Long qw(:config auto_version auto_help);
+use Getopt::Long qw(:config auto_version);
 use Pod::Usage;
 use Net::Telnet;
 use Fcntl;
@@ -660,7 +663,7 @@ GetOptions
     'mode=s' => sub { $opt_mode = str_to_int($_[1]) & 0xFFF; }
 ) or pod2usage(2);
 
-pod2usage(1) if $opt_help;
+pod2usage(-exitstatus => 0, -verbose => 99, -sections => 'SYNOPSIS|OPTIONS|COMMANDS') if $opt_help;
 pod2usage(-exitstatus => 0, -verbose => 2) if $opt_man;
 
 # Define command
@@ -697,8 +700,8 @@ if (!$q->open(Host => $opt_host, Port => $opt_port))
 
 # Wait for prompt and skip error message due to both linemode and echo Telnet options being refused
 if (!$q->waitfor('/^QCONN$/o') ||
-    !$q->waitfor(Match => $q->prompt, String => "error linemode-or-echo-not-supported\n") ||
-    !$q->waitfor(Match => $q->prompt, String => "error linemode-or-echo-not-supported\n"))
+    !$q->waitfor($q->prompt) ||
+    !$q->waitfor('/error linemode-or-echo-not-supported\s*/'))
 {
     print("Invalid or no response from QCONN server.\n");
     $q->close();
@@ -768,15 +771,74 @@ __END__
 
 =head1 NAME
 
-qcl.pl - QCONN client.
+qcl.pl - QNX QCONN service provider client.
 
 =head1 SYNOPSIS
 
-qcl.pl [options] [[--] command [parameters]]
+qcl.pl [I<options>] [[--] I<command> [I<parameters>]]
+
+=head1 COMMANDS
+
+=over 8
+
+=item B<info>
+
+Retrieves system information for the target the QCONN server is running on.
+This is the default command.
+
+=item B<versions>
+
+Retrieves a list of services supported by the QCONN server and their versions.
+
+=item B<exec> I<command> [I<parameters>]
+
+Executes the specified command on the target the QCONN server is running on.
+The use of "--" in front of the command is encouraged to avoid parameter misinterpretation.
+
+=item B<put> I<source> I<target>
+
+Transfers the file specified as source to the target the QCONN server is running on.
+The --mode option may be used to specify permissions for the file when it is created.
+
+=item B<get> I<source> I<target>
+
+Transfers the file specified as source from the target the QCONN server is running on.
+
+=item B<rm> I<target>
+
+Deletes the specified file or directory on the target the QCONN server is running on.
+
+=item B<mkdir> I<target>
+
+Creates the specified directory on the target the QCONN server is running on.
+
+=item B<chmod> I<target>
+
+Changes the permissions of the specified file or directory on the target the QCONN server
+is running on. The permissions are specified with the --mode option.
+
+=item B<kill> I<pid> I<signal>
+
+Sends the specified signal to the process with the specified PID on the target the QCONN
+server is running on.
+
+=back
 
 =head1 OPTIONS
 
 =over 8
+
+=item B<--host>=I<address>
+
+Specifies QCONN server as hostname or IP address. Default is localhost.
+
+=item B<--port>=I<port>
+
+Specifies QCONN server TCP port. Default is 8000.
+
+=item B<--mode>=I<permissions>
+
+Specifies file permissions used with some commands. Default is 0666.
 
 =item B<--version>
 
@@ -794,6 +856,90 @@ Prints detailed program documentation.
 
 =head1 DESCRIPTION
 
-Under development.
+qcl.pl is a Perl script working as a QNX QCONN service provider client. It allows
+controlling a QCONN server from the command-line and automating certain development
+activities for the QNX platform without using additional server applicaitons (e.g.
+Telnet, FTP, SSH, etc.).
+
+This script is far from complete and is not intended to replace the use of the
+QNX Momentics IDE.
+
+The Net::Telnet Perl module is required (L<http://search.cpan.org/~jrogers/Net-Telnet-3.03/lib/Net/Telnet.pm>).
+
+=head1 EXAMPLES
+
+Transferring a file to a QNX target:
+
+    C:\qcl>perl qcl.pl --host=10.0.0.2 put test.zip /tmp
+
+Retrieving a file from a QNX target:
+
+    C:\qcl>perl qcl.pl --host=10.0.0.2 get /tmp/test.zip .
+
+Transferring an application to a QNX target and executing it:
+
+    C:\qcl>perl qcl.pl --host=10.0.0.2 --mode=0777 put hello /tmp
+    C:\qcl>perl qcl.pl --host=10.0.0.2 exec /tmp/hello
+    Hello World!
+
+Executing an application on a QNX target:
+
+    C:\qcl>perl qcl.pl --host=10.0.0.2 -- exec /bin/ls -l /usr
+    total 168
+    drwxrwxr-x 14 root      root           4096 Jun 04  2008 .
+    drwxr-xr-x 17 root      root           4096 Mar 27 15:25 ..
+    drwxrwxr-x  2 root      root          16384 Apr 15  2008 bin
+    drwxrwxr-x  3 root      root           4096 May 28  2007 help
+    drwxrwxr-x 35 root      root          12288 Aug 08  2008 include
+    drwxrwxr-x  6 root      root          12288 Aug 08  2008 lib
+    drwxrwxr-x  2 root      root           4096 May 28  2007 libexec
+    drwxrwxr-x 13 root      root           4096 May 14  2008 local
+    drwxrwxr-x  3 root      root           4096 May 28  2007 man
+    drwxrwxr-x 16 root      root           4096 Sep 10  2008 photon
+    drwxrwxr-x  6 root      root           4096 May 28  2007 qnx630
+    drwxrwxr-x  2 root      root           4096 Oct 23  2007 sbin
+    drwxrwxr-x 14 root      root           4096 May 28  2007 share
+
+Retrieving system information for a QNX target:
+
+    C:\qcl>perl qcl.pl --host=10.0.0.2 info
+    ARCHITECTURE=x86
+    CPU=x86
+    ENDIAN=le
+    HASVERSION=1
+    HOSTNAME=localhost
+    MACHINE=x86pc
+    NUM_SRVCS=1
+    OS=nto
+    RELEASE=6.3.2
+    SYSNAME=QNX
+    TIMEZONE=EET-02EEST-03,M3.5.0/2,M10.5.0/2
+    VERSION=2006/03/16-14:19:50EST
+
+=head1 AUTHOR
+
+Kaloyan Tenchov, L<http://kaloyan.tenchov.name/>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2009 Kaloyan Tenchov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
 =cut
